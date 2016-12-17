e8924704d396762fdfb4e744e5a0a2c549b56b57
Varalex <alex.varkalov@gmail.com>
2016-12-18 02:01:30 +0300

    refactor stream image. Reduce logging.

--- src/stream_image.py
+++ src/stream_image.py
@@ -3,185 +3,106 @@ import arrow
 import netCDF4 as nc4
 import numpy as np
 import matplotlib.pyplot as plt
-import Tkinter
-import tkFileDialog
 from matplotlib import cm
 from utils import *
 
 
-def horizontal_cut(paths, folder, log_file):
+def horizontal_cut(paths, folder):
     step = 1
-
-    log_file.writelines('step = {} \n'.format(step))
-    log_file.writelines('+++++++++++++++++++++++\n')
-    log_file.writelines('Horizontal cut\n')
-    log_file.writelines('+++++++++++++++++++++++\n')
-
+    start_height, end_height = None, None
+    work_folder = '{}/{}'.format(folder, arrow.now().now().strftime("%Y-%m-%d_%H:%M:%S"))
+    os.makedirs(work_folder)
+    divide_method = get_divide_method()
     for path in paths:
-        file_start_time = arrow.now()
-        file_name = path_leaf(path)
-
-        log_file.writelines('-----------------------\n')
-        log_file.writelines('File {} Start Time - {}\n\n'.format(file_name, file_start_time))
-        log_file.writelines('-----------------------\n')
-
-        print 'File {} Start Time - {}'.format(file_name, file_start_time)
-        file_folder = file_name.split('.')[0]
-        os.makedirs('{}/{}'.format(folder, file_folder))
-        # print path
         nc_file = nc4.Dataset(path, mode='r')
+        hour = str(arrow.get(''.join(nc_file.variables['Times'][0]), 'YYYY-M-D_HH:mm:ss').time().hour).zfill(2)
         max_level = nc_file.variables['U'].shape[1] - 1
-        while True:
-            try:
-                highs = input('Enter High levels. Min is 0. Max is {}. Write -1 for all: '.format(max_level))
-            except Exception:
-                print('Wrong Input')
-                continue
-            if highs == -1:
-                start_high = 0
-                end_high = max_level
-                break
-            elif isinstance(highs, int) and 0 <= highs <= max_level:
-                start_high = highs
-                end_high = highs + 1
-                break
-            elif isinstance(highs, tuple) and len(highs) == 2 and highs[1] > highs[0] >= 0:
-                start_high = highs[0]
-                end_high = highs[1]
-                break
-            else:
-                print('Wrong Input')
-                continue
+        if not start_height and not end_height:
+            start_height, end_height = get_heights(max_level=max_level)
 
-        for h in range(start_high, end_high):
-            high_start_time = arrow.now()
-            log_file.writelines('High #{} Start Time - {}\n'.format(h, high_start_time))
-            x_wind = nc_file.variables['U']
-            y_wind = nc_file.variables['V']
-            x_wind0 = x_wind[0][h]
-            y_wind0 = y_wind[0][h]
-            min_shape = min(x_wind0.shape[0], x_wind0.shape[1], y_wind0.shape[0], y_wind0.shape[1])
-            x_wind0 = x_wind0[:min_shape, :min_shape]
-            y_wind0 = y_wind0[:min_shape, :min_shape]
-            X, Y = np.meshgrid(np.arange(0, x_wind0.shape[0], 1), np.arange(0, x_wind0.shape[1], 1))
-            U = x_wind0
-            V = y_wind0
-            Z = create_module_matrix(U, V)
+        for height in range(start_height, end_height):
+            U = nc_file.variables['U'][0][height]
+            V = nc_file.variables['V'][0][height]
+            min_shape = min(U.shape[0], U.shape[1], V.shape[0], V.shape[1])
+            U = U[:min_shape, :min_shape]
+            V = V[:min_shape, :min_shape]
+            X = nc_file.variables['XLONG'][0]
+            Y = nc_file.variables['XLAT'][0]
+            color = create_module_matrix(U, V)
             plt.figure(figsize=(12.5, 10), facecolor='w', edgecolor='r')
             plt.streamplot(
                 X[::step, ::step],
                 Y[::step, ::step],
                 U[::step, ::step],
                 V[::step, ::step],
-                color=Z[::step, ::step],
+                color=color[::step, ::step],
                 cmap=cm.cool,
                 linewidth=1.0,
                 arrowstyle='->',
                 arrowsize=1.5,
             )
             plt.colorbar()
-            plt.axis([-10, 260, -10, 260])
-            plt.axis([-X.shape[1] * 0.1, X.shape[1] * 1.1, -Y.shape[0] * 0.1, Y.shape[0] * 1.1])
-            plt.title('{}/high{}.png'.format(folder, h))
-            plt.savefig('{}/{}/high{}.png'.format(folder, file_folder, h))
+            if divide_method == 'hours':
+                if not os.path.exists('{}/hours'.format(work_folder)):
+                    os.makedirs('{}/hours'.format(work_folder))
+                if not os.path.exists('{}/hours/{}'.format(work_folder, hour)):
+                    os.makedirs('{}/hours/{}'.format(work_folder, hour))
+                plt.title('{}/hours/{}/height#{}'.format(work_folder, hour, str(height).zfill(2)))
+                plt.savefig('{}/hours/{}/{}.png'.format(work_folder, hour, str(height).zfill(2)))
+            elif divide_method == 'heights':
+                if not os.path.exists('{}/heights'.format(work_folder)):
+                    os.makedirs('{}/heights'.format(work_folder))
+                if not os.path.exists('{}/heights/{}'.format(work_folder, str(height).zfill(2))):
+                    os.makedirs('{}/heights/{}'.format(work_folder, str(height).zfill(2)))
+                plt.title('{}/heights/{}/hour-{}'.format(work_folder, str(height).zfill(2), hour))
+                plt.savefig('{}/heights/{}/{}.png'.format(work_folder, str(height).zfill(2), hour))
             plt.close()
 
-            high_end_time = arrow.now()
-            high_delta = high_end_time - high_start_time
-            print 'High #{} Delta - {}'.format(h, high_delta)
-            log_file.writelines('High End Time - {}\n'.format(high_end_time))
-            log_file.writelines('High Delta - {}\n\n'.format(high_delta))
-        file_end_time = arrow.now()
-        log_file.writelines('-----------------------\n')
-        log_file.writelines('File {} End Time - {}\n\n'.format(file_name, file_end_time))
-        print 'File {} End Time - {}\n'.format(file_name, file_end_time)
-        file_delta = file_end_time - file_start_time
-        log_file.writelines('File {} Delta - {}\n'.format(file_name, file_delta))
-        log_file.writelines('-----------------------\n')
-        print 'High {} Delta - {}\n'.format(file_name, file_delta)
 
-
-def vertical_cut(paths, folder, log_file):
+def vertical_cut(paths, folder):
     step = 1
-
-    log_file.writelines('step = {} \n'.format(step))
-    log_file.writelines('+++++++++++++++++++++++\n')
-    log_file.writelines('Vertical cut\n')
-    log_file.writelines('+++++++++++++++++++++++\n')
-
+    pointA, pointB = None, None,
+    sin_alpha, cos_alpha = None, None
+    start_height, end_height = None, None
+    coords = []
+    work_folder = '{}/{}'.format(folder, arrow.now().now().strftime("%Y-%m-%d_%H:%M:%S"))
+    os.makedirs(work_folder)
     for path in paths:
-        file_start_time = arrow.now()
-        file_name = path_leaf(path)
-
-        log_file.writelines('-----------------------\n')
-        log_file.writelines('File {} Start Time - {}\n\n'.format(file_name, file_start_time))
-        log_file.writelines('-----------------------\n')
-        print 'File {} Start Time - {}'.format(file_name, file_start_time)
-
-        file_folder = file_name.split('.')[0]
-        os.makedirs('{}/{}'.format(folder, file_folder))
         nc_file = nc4.Dataset(path, mode='r')
-        while True:
-            print('Enter Points coordinates')
-            max_value = min(nc_file.variables['U'][0][0].shape[0], nc_file.variables['U'][0][0].shape[1])
-            pointA = raw_input('point A. Min - (0,0). Max({},{}): '.format(max_value - 1, max_value - 1)).split(', ')
-            if len(pointA) > 2:
-                print('Wrong Input')
-                continue
-            else:
-                pointA[0] = int(pointA[0])
-                pointA[1] = int(pointA[1])
-                if pointA[0] >= max_value or pointA[1] >= max_value:
-                    print('Wrong Input')
-                    continue
-            pointB = raw_input('point B. Min - (0,0). Max({},{}): '.format(max_value - 1, max_value - 1)).split(', ')
-            if len(pointB) > 2:
-                print('Wrong Input')
-                continue
-            else:
-                pointB[0] = int(pointB[0])
-                pointB[1] = int(pointB[1])
-                if pointB[0] >= max_value or pointB[1] >= max_value:
-                    print('Wrong Input')
-                    continue
-            break
-        if pointB[0] < pointA[0]:
-            pointA, pointB = pointB, pointA
-        if pointA[0] - pointB[0] == 0:
-            sin_alpha = 1
-            cos_alpha = 10 ** 13
-        elif pointA[1] - pointB[1] == 0:
-            sin_alpha = 10 ** 13
-            cos_alpha = 1
-        else:
-            sin_alpha = (pointB[1] - pointA[1]) / np.sqrt((pointB[0] - pointA[0]) ** 2 + (pointB[1] - pointA[1]) ** 2)
-            cos_alpha = np.sqrt(1 - sin_alpha ** 2)
-
-        coords = bresenham_line(pointA, pointB)
-        matrixXY = []
-        matrixZ = []
-        for i in range(27):
-            U = nc_file.variables['U'][0][i]
-            V = nc_file.variables['V'][0][i]
-            W = nc_file.variables['W'][0][i]
-            vectorXY = []
-            vectorZ = []
+        hour = str(arrow.get(''.join(nc_file.variables['Times'][0]), 'YYYY-M-D_HH:mm:ss').time().hour).zfill(2)
+        max_value = min(nc_file.variables['U'][0][0].shape[0], nc_file.variables['U'][0][0].shape[1])
+        if not pointA or not pointB or not sin_alpha or not cos_alpha or not coords:
+            pointA, pointB = set_points(max_value=max_value)
+            sin_alpha, cos_alpha = get_sin_cos_by_points(pointA=pointA, pointB=pointB)
+            coords = bresenham_line(pointA, pointB)
+        max_level = nc_file.variables['U'].shape[1] - 1
+        if not start_height and not end_height:
+            start_height, end_height = get_heights(max_level=max_level)
+        UVp = []
+        Wp = []
+        for height in range(start_height, end_height):
+            U = nc_file.variables['U'][0][height]
+            V = nc_file.variables['V'][0][height]
+            W = nc_file.variables['W'][0][height]
+            # X = nc_file.variables['XLONG'][0]
+            # Y = nc_file.variables['XLAT'][0]
+            vectorUVp = []
+            vectorWp = []
             for x, y in coords:
-                vectorXY.append(U[x][y] / cos_alpha + V[x][y] / sin_alpha)
-                vectorZ.append(W[x][y])
-            matrixXY.append(vectorXY)
-            matrixZ.append(vectorZ)
-
-        matrixXY = np.array(matrixXY)
-        matrixZ = np.array(matrixZ)
-        X, Y = np.meshgrid(np.arange(0, matrixXY.shape[1], 1), np.arange(0, matrixXY.shape[0], 1))
-        Z = create_module_matrix(matrixXY, matrixZ)
+                vectorUVp.append(U[x][y] / cos_alpha + V[x][y] / sin_alpha)
+                vectorWp.append(W[x][y])
+            UVp.append(vectorUVp)
+            Wp.append(vectorWp)
+        UVp = np.array(UVp)
+        Wp = np.array(Wp)
+        X, Y = np.meshgrid(np.arange(0, UVp.shape[1], 1), np.arange(start_height, end_height, 1))
+        Z = create_module_matrix(UVp, Wp)
         plt.figure(figsize=(20, 10), facecolor='w', edgecolor='r')
         plt.streamplot(
             X[::step, ::step],
             Y[::step, ::step],
-            matrixXY[::step, ::step],
-            matrixZ[::step, ::step],
+            UVp[::step, ::step],
+            Wp[::step, ::step],
             color=Z[::step, ::step],
             cmap=cm.cool,
             linewidth=1.0,
@@ -189,55 +110,36 @@ def vertical_cut(paths, folder, log_file):
             arrowsize=1.5,
         )
         plt.colorbar()
-        plt.axis([-matrixXY.shape[1] * 0.1, matrixXY.shape[1] * 1.1, -matrixXY.shape[0] * 0.1, matrixXY.shape[0]])
-        plt.title('{}/A({},{});B({},{}).png'.format(folder, pointA[0], pointA[1], pointB[0], pointB[1]))
-        plt.savefig(
-            '{}/{}/A({},{});B({},{}).png'.format(folder, file_folder, pointA[0], pointA[1], pointB[0], pointB[1]))
+        plt.title('{}/{}.png'.format(work_folder, hour))
+        plt.savefig('{}/{}.png'.format(work_folder, hour))
         plt.close()
-
-        file_end_time = arrow.now()
-        log_file.writelines('-----------------------\n')
-        log_file.writelines('File {} End Time - {}\n\n'.format(file_name, file_end_time))
-        print 'File {} End Time - {}\n'.format(file_name, file_end_time)
-        file_delta = file_end_time - file_start_time
-        log_file.writelines('File {} Delta - {}\n'.format(file_name, file_delta))
-        log_file.writelines('-----------------------\n')
-        print 'High {} Delta - {}\n'.format(file_name, file_delta)
 
 
 def main():
-    directory = '../StreamImage'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    folder = '../StreamImage'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     start_time = arrow.now()
-    folder = '{}/StreamImages#{}'.format(directory, start_time.timestamp)
-    os.makedirs(folder)
-    with open('{}/log.txt'.format(folder), mode='w', ) as log_file:
-        log_file.write('***********************\n')
-        log_file.write('Start Time - {}\n'.format(start_time))
-        log_file.write('***********************\n')
-
-        # paths = get_file_paths('nc')
-        paths = ['/home/varalex/University/nc/wrfout_d01_2013-05-22_01.nc', ]
-        while True:
-            print '1. Horizontal cut'
-            print '2. Vertical cut'
+    paths = get_file_paths('nc')
+    while True:
+        print '1. Horizontal cut'
+        print '2. Vertical cut'
+        try:
             choise = input()
-            if choise == 1:
-                horizontal_cut(paths=paths, folder=folder, log_file=log_file)
-                break
-            elif choise == 2:
-                vertical_cut(paths=paths, folder=folder, log_file=log_file)
-                break
-            else:
-                continue
-
-        end_time = arrow.now()
-        work_time = end_time - start_time
-        log_file.write('***********************\n')
-        log_file.write('End Time - {}\n'.format(end_time))
-        log_file.write('Work Time - {}\n'.format(work_time))
-        log_file.write('***********************\n')
+        except Exception:
+            print('Wrong Input')
+            continue
+        if choise == 1:
+            horizontal_cut(paths=paths, folder=folder)
+            break
+        elif choise == 2:
+            vertical_cut(paths=paths, folder=folder)
+            break
+        else:
+            print('Wrong Input')
+            continue
+    end_time = arrow.now()
+    work_time = end_time - start_time
 
 
 main()
--- src/utils.py
+++ src/utils.py
@@ -92,3 +92,96 @@ def create_module_matrix(X, Y):
         for j in range(Z.shape[1]):
             Z[i][j] = np.sqrt(sq_X[i][j] + sq_Y[i][j])
     return Z
+
+
+def get_heights(max_level):
+    while True:
+        try:
+            heights = input('Enter High levels.\n'
+                            'Min is 0. Max is {}. Write -1 for all\n'
+                            'Example: 1, 10\n'
+                            ': '.format(max_level)
+                            )
+        except Exception:
+            print('Wrong Input')
+            continue
+        if heights == -1:
+            start_high = 0
+            end_high = max_level
+            break
+        elif isinstance(heights, int) and 0 <= heights <= max_level:
+            start_high = heights
+            end_high = heights + 1
+            break
+        elif isinstance(heights, tuple) and len(heights) == 2 and heights[1] > heights[0] >= 0:
+            start_high = heights[0]
+            end_high = heights[1]
+            break
+        else:
+            print('Wrong Input')
+            continue
+    return start_high, end_high
+
+
+def get_divide_method():
+    while True:
+        print('Divide by heights or hours?')
+        print('1. Heights\n2. Hours')
+        try:
+            choose = input(': ')
+        except Exception:
+            print('Wrong Input')
+            continue
+        if choose == 1:
+            return 'heights'
+        elif choose == 2:
+            return 'hours'
+        else:
+            print('Wrong Input')
+            continue
+
+
+def set_points(max_value):
+    while True:
+        try:
+            print('Enter Points coordinates')
+            pointA = raw_input('point A. Min - (0,0). Max({},{}): '.format(max_value - 1, max_value - 1)).split(', ')
+            if len(pointA) > 2:
+                print('Wrong Input')
+                continue
+            else:
+                pointA[0] = int(pointA[0])
+                pointA[1] = int(pointA[1])
+                if pointA[0] >= max_value or pointA[1] >= max_value:
+                    print('Wrong Input')
+                    continue
+            pointB = raw_input('point B. Min - (0,0). Max({},{}): '.format(max_value - 1, max_value - 1)).split(', ')
+            if len(pointB) > 2:
+                print('Wrong Input')
+                continue
+            else:
+                pointB[0] = int(pointB[0])
+                pointB[1] = int(pointB[1])
+                if pointB[0] >= max_value or pointB[1] >= max_value:
+                    print('Wrong Input')
+                    continue
+            if pointB[0] < pointA[0]:
+                pointA, pointB = pointB, pointA
+            return pointA, pointB
+        except Exception:
+            print('Wrong Input')
+            continue
+
+
+def get_sin_cos_by_points(pointA, pointB):
+    if pointA[0] - pointB[0] == 0:
+        sin_alpha = 1
+        cos_alpha = 10 ** 13
+    elif pointA[1] - pointB[1] == 0:
+        sin_alpha = 10 ** 13
+        cos_alpha = 1
+    else:
+        sin_alpha = (pointB[1] - pointA[1]) / np.sqrt((pointB[0] - pointA[0]) ** 2 + (pointB[1] - pointA[1]) ** 2)
+        cos_alpha = np.sqrt(1 - sin_alpha ** 2)
+    return sin_alpha, cos_alpha
+
